88bdbe0bff7ac285ca08ba217fcdcae24b299b7a
Maxim Grushevsky <m.grushevsky10@gmail.com>
2018-11-04 21:48:27 +0300

    updated portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     const navigation = $(".navigation");
+    const navbar = $(".container");
     let scroll = $(window).scrollTop();
   
     if (scroll === 0) {
       navigation.addClass("navbar-transparent");
+      navbar.removeClass("navbar-text-transparent");
     } else {
       navigation.removeClass("navbar-transparent");
+      navbar.addClass("navbar-text-transparent");
     }
   
     $(window).scroll(function() {
@@ -13,8 +16,10 @@ $(document).ready(function() {
   
       if (scroll >= 200) {
         navigation.removeClass("navbar-transparent");
+        navbar.addClass("navbar-text-transparent");
       } else {
         navigation.addClass("navbar-transparent");
+        navbar.removeClass("navbar-text-transparent");
       }
     });
   
